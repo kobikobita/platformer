671376b8eb2e8d19cbb2ec0b855e117557ceb423
kobikobita <kokobikobita@gmail.com>
2026-07-02 21:08:07 +0600

    changing scenes and end scenes!!!

---
+++ Scenes/toWin.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export_file("*.tscn") var target_scene: String
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Nicole":
+		get_tree().change_scene_to_file("res://Scenes/Win.tscn")
